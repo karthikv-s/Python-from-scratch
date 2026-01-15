0b8f1ff3ce2dc275ebc6d6c2b4b8b5f05c93f0d2
Karthik V S <karthikv.s2024@lpu.in>
2026-01-16 04:06:47 +0530

    Add files via upload
    I learned about math functions and if and else statements. I just realised that i have forgot all these things,but slowly,am picking it again.

---
+++ 10 if statements .py
@@ -0,0 +1,19 @@
+# is_hot=False
+# is_cold=False
+# if is_hot:
+#     print("Its a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("Its a cold day")
+#     print("Wear warm cloths")
+# else:
+#     print("Its a lovely day")
+# print("Enjoy your day")
+
+price=1000000
+good_credit=False
+if good_credit:
+    down_payment=(price*10/100)
+else:
+    down_payment=(price*20/100)
+print(f"Down payment is: {down_payment}")
---
+++ 9 math_functions.py
@@ -0,0 +1,10 @@
+# x=2.9
+# print(round(x))
+# y=2.4
+# print(round(y))
+# x=2.9
+# print(abs(-2.9))
+
+import math
+print(math.ceil(2.9))
+print(math.floor(2.9))
