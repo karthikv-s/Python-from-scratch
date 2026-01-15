596b7efb2ab8b1a06320411ee543a68ff0b331bf
Karthik V S <karthikv.s2024@lpu.in>
2026-01-15 22:49:08 +0530

    Add files via upload
    Arithmetic Operations and Operator preceedence

---
+++ 8 arithmetic_operation.py
@@ -0,0 +1,15 @@
+# print(10/3)
+# print(10+3)
+# print(10//3)
+# print(10-3)
+# print(10*3)
+# print(10%3)
+# print(10**3)
+
+# x=10
+# x=x+3
+# x=x-3
+# print(x)
+x=input("Ener a number:")
+
+print(x)
