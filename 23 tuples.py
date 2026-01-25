2cf92747a109e2c0cb2355839409bd23504377fa
Karthik V S <karthikv.s2024@lpu.in>
2026-01-25 22:22:07 +0530

    Add files via upload

---
+++ 23 tuples.py
@@ -0,0 +1,11 @@
+# //tuples are imutable
+# numbers=(1,2,3,4,5)
+# numbers.count(2)
+# print(numbers.count(2))
+
+#unpacking of tuples
+coordinates=(1,2,3)
+x,y,z=coordinates
+print(x)
+print(y)
+print(z)
