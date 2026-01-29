0e5c67efc3b12f6a4c7552a6c68283fdc5927c24
Karthik V S <karthikv.s2024@lpu.in>
2026-01-29 19:23:31 +0530

    Add files via upload
    Parameters and arguments

---
+++ 27 parameters.py
@@ -0,0 +1,9 @@
+def greet_user(name):
+    print(f"Hi {name}")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user("Karthik")
+greet_user("Aswanth")
+print("Finish")
---
+++ 28 arguments.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user("Karthik","V S")
+greet_user("Aswanth","Manoj")
+print("Finish")
