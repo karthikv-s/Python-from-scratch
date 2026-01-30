6a353aa3a7f6ee68047d704e0e77faefed4d7a17
Karthik V S <karthikv.s2024@lpu.in>
2026-01-30 22:48:42 +0530

    Add files via upload
    Return statements

---
+++ 29 return.py
@@ -0,0 +1,10 @@
+def square(number):
+    return number * number
+
+def cube(number):
+    return number*number*number
+
+
+value=int(input("Enter a number for cube: "))
+print("Square of the number is:", square(value))
+print("Cube of the number is:", cube(value))
