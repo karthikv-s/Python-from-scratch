52d1b16564ae75e843ed84d62a01fda54b557a15
Karthik V S <karthikv.s2024@lpu.in>
2026-02-08 14:31:22 +0530

    Add files via upload
    Started with classes,one of the most important topics

---
+++ 33 classes.py
@@ -0,0 +1,17 @@
+class Point:
+    def move(self):
+        print("Move")
+    def draw(sef):
+        print("Draw")
+
+point1 = Point()
+point1.move()
+point1.x=10
+point1.y=20
+print(point1.x)
+print(point1.y)
+point1.draw()
+
+point2=Point()
+point2.x=1
+print(point2.x)
